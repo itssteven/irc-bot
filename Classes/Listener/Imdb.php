66a5e82210e7cb3d0833c7b634122d0d84727851
itssteven <dunno@gmail.com>
2016-05-12 18:21:16 +0100

    imdb listen fix

--- Classes/Listener/Imdb.php
+++ Classes/Listener/Imdb.php
@@ -30,48 +30,39 @@ class Imdb extends \Library\IRC\Listener\Base {
         
         foreach( $args as $arg ) {
         	if( $listenee = strstr( $arg, "imdb.com/title/" ) ) {
-				
-				// Google search the url
-				$query_url = 'https://ajax.googleapis.com/ajax/services/search/web?v=1.0&q=' . $arg;
-        
-				$ch = curl_init();
-				curl_setopt( $ch, CURLOPT_URL, $query_url );
-				curl_setopt( $ch, CURLOPT_RETURNTRANSFER, 1 );
-				$query_return = curl_exec( $ch );
-				curl_close( $ch );
-				$decoded = json_decode( $query_return, true );
-				
-				if( isset( $decoded['responseData']['results'][0]['title'] ) &&
-					isset( $decoded['responseData']['results'][0]['url'] ) ) {
-					$url 	= $decoded['responseData']['results'][0]['url'];
-					$url   = urldecode( $url );
-					
-					// Get movie id from url
-					$parsed_url = parse_url( $url );
-					$movie_id = substr( $parsed_url['path'], strlen( '/title/' ) );
-					$movie_id = substr( $movie_id, 0, stripos( $movie_id, '/' ) );
-					
-					// Get movie info via omdb's API
-					$query = 'http://www.omdbapi.com/?i=' . $movie_id;	       	
-					$ch = curl_init();
-					curl_setopt( $ch, CURLOPT_URL, $query );
-					curl_setopt( $ch, CURLOPT_RETURNTRANSFER, 1 );
-					$query_ret = curl_exec( $ch );
-					curl_close( $ch );
-					
-					$decoded = json_decode( $query_ret, true );
-					
-					if( isset( $decoded['Error'] ) ) 
-						return;
-					
-					$movie_title 	= $decoded['Title'];
-					$movie_year 	= $decoded['Year'];
-					$movie_plot 	= $decoded['Plot'];
-					$movie_rating 	= $decoded['imdbRating'];
-					
-					$this->say( "1,8IMDb $movie_title ($movie_year) $movie_rating - $movie_plot $url", $args[2] );
-					break;
+				        
+				$ret = google( $arg );
+				if( $ret === FALSE ) {
+					echo '('."imdb $query_string".') returned false...', PHP_EOL;
+					return;
 				}
+				$url = $ret['url'];				
+					
+				// Get movie id from url
+				$parsed_url = parse_url( $url );
+				$movie_id = substr( $parsed_url['path'], strlen( '/title/' ) );
+				$movie_id = substr( $movie_id, 0, stripos( $movie_id, '/' ) );
+				
+				// Get movie info via omdb's API
+				$query = 'http://www.omdbapi.com/?i=' . $movie_id;	       	
+				$ch = curl_init();
+				curl_setopt( $ch, CURLOPT_URL, $query );
+				curl_setopt( $ch, CURLOPT_RETURNTRANSFER, 1 );
+				$query_ret = curl_exec( $ch );
+				curl_close( $ch );
+				
+				$decoded = json_decode( $query_ret, true );
+				
+				if( isset( $decoded['Error'] ) ) 
+					return;
+				
+				$movie_title 	= $decoded['Title'];
+				$movie_year 	= $decoded['Year'];
+				$movie_plot 	= $decoded['Plot'];
+				$movie_rating 	= $decoded['imdbRating'];
+				
+				$this->say( "1,8IMDb $movie_title ($movie_year) $movie_rating - $movie_plot $url", $args[2] );
+				break;
 			}       
 		} 
     }
